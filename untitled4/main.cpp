216cfc73948aa9bd80ed16bf030b610294e7d9e1
DiogoHiebert <132789512+DiogoHiebert@users.noreply.github.com>
2023-05-07 17:27:01 -0300

    Update main.cpp

--- untitled4/main.cpp
+++ untitled4/main.cpp
@@ -9,7 +9,13 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow window;
 
+    QString qssFilePath = ":/assets/img/styles.qss";
+//    QString qssFilePath = QCoreApplication::applicationDirPath() + "/styles.qss";
+    QFile qssFile( qssFilePath );
+    qssFile.open(QFile::ReadOnly);
+    QString styleSheet = QLatin1String(qssFile.readAll());
 
+    a.setStyleSheet(styleSheet);
 
 
 
